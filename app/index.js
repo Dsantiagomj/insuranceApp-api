d5643c3e68e7f64e035f02137e71de4336436235
Daniel Munoz <dsantiagomj@gmail.com>
2024-10-10 11:24:55 -0500

    organize express app and create first endpoint

--- app/index.js
+++ app/index.js
@@ -1,7 +1,30 @@
 import express from "express";
+import { router as api } from "./api/v1/index.js";
 
 export const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+// Parse JSON
+app.use(express.json());
+
+// Setup router and routes
+app.use("/api/v1", api);
+app.use("/api", api);
+
+// No route found handler
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: "Error. Route not found",
+  });
+});
+
+// Error handler
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { status = 500, message } = err;
+
+  res.status(status);
+  res.json({
+    error: { status, message },
+  });
 });
