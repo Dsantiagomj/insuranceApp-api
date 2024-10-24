52ad25be363523df1603fe7ba0c49a3c216fb3e0
Daniel Munoz <dsantiagomj@gmail.com>
2024-10-24 18:51:51 -0500

    database connection debug

--- app/database.js
+++ app/database.js
@@ -5,16 +5,24 @@ const client = new SecretManagerServiceClient();
 export const prisma = new PrismaClient();
 
 export async function getDbUrl() {
-  const [version] = await client.accessSecretVersion({
-    name: "projects/insuranceapp-438123/secrets/DATABASE_URL/versions/latest",
-  });
-  return version.payload.data.toString("utf8");
+  try {
+    const [version] = await client.accessSecretVersion({
+      name: "projects/insuranceapp-438123/secrets/DATABASE_URL/versions/latest",
+    });
+    return version.payload.data.toString("utf8");
+  } catch (error) {
+    console.error("Error retrieving DATABASE_URL:", error);
+    throw error; // Rethrow to handle in the connect function
+  }
 }
 
 export const connect = async function () {
   const dbUrl = await getDbUrl();
+  console.log("Retrieved DATABASE_URL:", dbUrl);
   // Set the DATABASE_URL for Prisma
   process.env.DATABASE_URL = dbUrl;
+  // Optionally log the database URL (for debugging)
+  console.log("DATABASE_URL:", process.env.DATABASE_URL);
   await prisma.$connect();
   console.log("Database connected");
 };
